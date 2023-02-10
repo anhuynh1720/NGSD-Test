19509ec77c8992e3bc6e606754719ba8240f1132
Huynh Ngoc An <huynhngocan1720@gmail.com>
2023-02-10 12:06:49 +0700

    Change routes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 const router = require('./app/routes/router');
 
-app.use("", router);
+app.use("/", router);
 
 app.listen(5000, () => {
     console.log("Server has started on port 5000");
